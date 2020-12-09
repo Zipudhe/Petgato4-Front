9bf947dcbe2e5f3975870c097b28ca63ceb52d92
map3101 <marcelo.postiglione@gmail.com>
2020-12-08 23:22:20 -0300

    fix: corrige responsividade do elemento input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,7 +3,7 @@ import './style.css';
 export default function Input({name, disabled=false, textholder}){
     return (
         <div className="input">
-            <label id="form-label" for="form-input">{name}</label><br></br>
+            <label id="form-label" htmlFor="form-input">{name}</label>
             <input placeholder={textholder} disabled={disabled} id="form-input"/>
         </div>
     )
