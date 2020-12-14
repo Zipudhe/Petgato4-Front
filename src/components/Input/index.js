62c64d1121b437269c2a54f4c3bb7362cdfe6598
map3101 <48570991+map3101@users.noreply.github.com>
2020-12-14 14:13:38 -0300

    fix: corrige htmlfor do label
    Co-authored-by: Zipudhe <33942742+Zipudhe@users.noreply.github.com>

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,7 +3,7 @@ import './style.css';
 export default function Input({name, disabled=false, textholder}){
     return (
         <div className="input">
-            <label className="form-label">{name}</label>
+            <label className="form-label" htmlFor="{inputId}" >{name}</label>
             <input placeholder={textholder} disabled={disabled} className="form-input"/>
         </div>
     )
