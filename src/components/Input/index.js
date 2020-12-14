d427aa60d85003b790f3831b1719adde11a18622
map3101 <48570991+map3101@users.noreply.github.com>
2020-12-14 14:13:57 -0300

    fix: corrige id do input
    Co-authored-by: Zipudhe <33942742+Zipudhe@users.noreply.github.com>

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -4,7 +4,7 @@ export default function Input({name, disabled=false, textholder}){
     return (
         <div className="input">
             <label className="form-label" htmlFor="{inputId}" >{name}</label>
-            <input placeholder={textholder} disabled={disabled} className="form-input"/>
+            <input placeholder={textholder} disabled={disabled} id={inputId} className="form-input"/>
         </div>
     )
 }
