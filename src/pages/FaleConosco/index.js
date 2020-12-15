b5c815eba0a6540d4cfcb5d7a89e13a3c4193227
Victor Manuel <victormanuel@cjr.org.br>
2020-12-14 21:30:25 -0300

    feat: adiciona página de fale conosco

---
+++ src/pages/FaleConosco/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Header from '../../components/Header';
+//import Footer from './components/Footer';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import "./styles.css";
+import imagem_de_contato from "../../assets/images/Imagem de Contato.jpg";
+
+const FaleConosco = () => (
+    <div>
+        <Header />
+        <div className="container-enviar-mensagem">
+            <div className="dog-image">
+                <img src={imagem_de_contato} />
+            </div>
+
+            <div className="user-info">
+                <h2>FALE CONOSCO</h2>
+                <h1>Envie-nos uma mensagem de cão-tato</h1>
+                
+                <div className="container-input">
+                    <div className="name"><Input name="Nome" /></div>
+                    <Input name="Nome" />
+                </div>
+
+                <div className="input-message">
+                    <div className="message"><Input name="Mensagem" /></div>
+                </div>
+                <div className="container-button">
+                    <Button styles={1} onClick={() => ( alert('ENVIAR') )}>ENVIAR</Button>
+                </div>
+            </div>
+        </div>
+        {//<Footer />
+        }
+    </div>
+)
+
+export default FaleConosco;
