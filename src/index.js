432874a1d87b77ac68484854b6fc267c8c2d25a2
Victor Manuel <victormanuel@cjr.org.br>
2020-12-18 15:55:34 -0300

    estilo: adiciona imagens

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import PaginaInicial from './pages/PaginaInicial';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PaginaInicial />
   </React.StrictMode>,
   document.getElementById('root')
 );
