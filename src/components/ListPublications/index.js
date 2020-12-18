9ed5d19c2804b8afb1282c3611d8f8de5090c0a3
Victor Manuel <victormanuel@cjr.org.br>
2020-12-18 18:46:40 -0300

    fix: atualiza página ao deletar elemento

--- src/components/ListPublications/index.js
+++ src/components/ListPublications/index.js
@@ -1,41 +1,53 @@
 import './styles.css';
 
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, use } from 'react';
 
-export default function ListPublications({page}){
+export default function ListPublications({pageRef}){
+    const [page, setPage] = useState(pageRef);
     const [posts, setPosts] = useState([]);
-
-    function converteData(data){
-        data = data.split('T')[0].split('-').reverse()
-        return data.join('/');
-    }
+    
+    const converteData = (data) => (
+        data.split('T')[0].split('-').reverse().join('/')
+    );
 
     useEffect(() => {
         axios.get(`http://localhost:3000/posts?page=${page}`)
-        .then((response) => response.data)
-        .then((data) => setPosts(data));
-    }, [page]);
+            .then((response) => response.data)
+            .then((data) => setPosts(data))
+            .catch((error) => console.error(error));
+    }, [page, posts.length]);
     
+    /*function loadPosts(page) {
+        axios.get(`http://localhost:3000/posts?page=${page}`)
+            .then((response) => response.data)
+            .then((data) => setPosts(data))
+            .catch((error) => console.error(error));
+    }*/
+
     function handleDelete(post_id){
         if(window.confirm("Tem certeza?")){
-            axios.delete(`http://localhost:3000/posts/${post_id}`)
-            .then(alert("Post excluído!"))
-            .then(window.location.reload());
+            axios.delete(`http://localhost:3000/posts/${post_id}`);
+
+            //loadPosts(posts);
+            setPosts(posts);
         }
     }
 
     return (
         <table className="all-publications">
-            <tr>
-                <th>#</th>
-                <th>Data</th>
-                <th>Título</th>
-                <th></th>
-                <th></th>
-            </tr>
-            {posts.map((post) => 
-                (
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Data</th>
+                    <th>Título</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {posts.map((post) => 
+                    (
                     <tr key={post.id}>
                         <td>{post.id}</td>
                         <td>{converteData(post.created_at)}</td>
@@ -43,8 +55,9 @@ export default function ListPublications({page}){
                         <td><p>Editar</p></td>
                         <td><p onClick={() => handleDelete(post.id)}>Excluir</p></td>
                     </tr>
-                )
-            )}
+                    )
+                )}
+            </tbody>
         </table>
     );
 }
