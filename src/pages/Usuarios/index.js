71fe1e095a5221250f109761870474e55ff6ca26
Victor Manuel <victormanuel@cjr.org.br>
2020-12-17 22:00:31 -0300

    feat: cria página de usuários

---
+++ src/pages/Usuarios/index.js
@@ -0,0 +1,19 @@
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import ListUsers from '../../components/ListUsers';
+
+import './styles.css';
+
+export default function Usuarios({page}){
+    return (
+        <div>
+            <Header />
+            <div className="backoffice-usuarios">
+                <h2>BACKOFFICE</h2>
+                <h1>Usuários</h1>
+                <ListUsers />
+            </div>
+            <Footer />
+        </div>
+    );
+}
