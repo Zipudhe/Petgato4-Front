f7dabff3527c3821cd0964060eb4a0b2b29ceb06
Victor Manuel <victormanuel@cjr.org.br>
2020-12-17 23:46:36 -0300

    feat: adiciona o editor de texto

--- src/pages/NovaPublicacao/index.js
+++ src/pages/NovaPublicacao/index.js
@@ -3,11 +3,20 @@ import Footer from '../../components/Footer';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
 import './styles.css';
+import { ThemeProvider } from 'styled-components';
 
 export default function NovaPublicacao(){
     // preciso de todas as tags disponíveis aqui
     let tags = ['Adestramento', 'Aves', 'Adoção'];
+
+    function temp() {
+        var a = quill.getText(0, 10);
+        console.log(a);
+    }
 
     return (
         <div>
@@ -15,8 +24,8 @@ export default function NovaPublicacao(){
             <div className="backoffice-publicacao">
                 <h2>BACKOFFICE</h2>
                 <h1>Nova Publicação</h1>
-                <Input name="Título da Publicação"/>
-                <p>campo</p>
+                <div className="input-title"><Input name="Título da Publicação"/></div>
+                <div className="texto-publicacao"><ReactQuill theme="snow" /></div>
 
                 <h3>Escolha uma imagem de capa:</h3>
                 <p>Nenhum arquivo escolhido</p>
@@ -31,7 +40,7 @@ export default function NovaPublicacao(){
                 )
                 )}
 
-                <div className="gerenciar-tags"><Button onClick={() => alert('GERENCIAR TAGS')} styles="1">GERENCIAR TAGS</Button></div>
+                <div className="gerenciar-tags"><Button onClick={() => temp()} styles="1">GERENCIAR TAGS</Button></div>
                 <div className="footer-buttons">
                     oi
                 </div>
