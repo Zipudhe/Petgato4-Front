1eed89f1edad5467241a0d4711d383422bf94c0d
Victor Manuel <victormanuel@cjr.org.br>
2021-01-12 00:15:53 -0300

    fix: refatora para se ajustar a página e ser responsivo em desktops

--- src/components/PerfilHome/index.js
+++ src/components/PerfilHome/index.js
@@ -1,30 +1,30 @@
-import "./style.css";
+import './style.css';
 
-import autor from '../../assets/images/cintialorenzzo.jpeg'
 import { Link } from "@reach/router";
 
-import fb from "../../assets/awesome-facebook-square.svg";
-import ig from "../../assets/awesome-instagram.svg";
-import twt from "../../assets/awesome-twitter-square.svg";
+import cintia_lorenzzo from '../../assets/images/Cíntia Lorenzzo.jpg';
+import facebook_icon from '../../assets/awesome-facebook-square.svg';
+import instagram_icon from '../../assets/awesome-instagram.svg';
+import twitter_icon from '../../assets/awesome-twitter-square.svg';
 
 export default function PerfilHome({text, link, path}){
     return(
-        <div className="main-frame">
-            <img className="autor-pic" alt="Autora" src={autor}/>
-            <p>SOBRE A AUTORA</p>
-            <Link className="name-link" to="/">Cíntia Lorenzzo</Link>
-            <p className="about">Sou veterinária a 5 anos,<br></br> 
-                apaixonada pelo mundo animal,<br></br>
-                principalmente os que estão sempre conosco no dia a dia.<br></br>
-                Quando não estou no meu consultório com meus pets,<br></br>
-                estou aqui escrevendo conteúdo para vocês. Espero que você
-                <br></br> goste!
+        <div className="about-frame">
+            <img className="crop-image" alt="Cíntia Lorenzzo" src={cintia_lorenzzo} />
+            <h2>SOBRE A AUTORA</h2>
+            <h1>Cíntia Lorenzzo</h1>
+
+            <p>
+                Sou veterinária há 5 anos, apaixonada pelo mundo animal, 
+                principalmente os que estão sempre conosco no dia a dia. 
+                Quando não estou no meu consultório com meus pets, 
+                estou aqui escrevendo conteúdo para vocês. Espero que você goste!
             </p>
 
             <div className="social-media-icons">
-                <img alt="Facebook" src={fb}/>
-                <img alt="Instagram" src={ig}/>
-                <img alt="Twitter" src={twt}/>
+                <a href="https://www.facebook.com" target="_blank"><img alt="Facebook" src={facebook_icon} /></a>
+                <a href="https://www.instagram.com" target="_blank"><img alt="Instagram" src={instagram_icon} /></a>
+                <a href="https://www.twitter.com" target="_blank"><img alt="Twitter" src={twitter_icon} /></a>
             </div>
         </div>
     )
