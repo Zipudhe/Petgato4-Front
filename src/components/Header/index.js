586215dfb346500735b7a58c6c5d20601fef83e1
Victor Manuel <victormanuel@cjr.org.br>
2021-01-12 15:38:54 -0300

    fix: corrige bug nas rotas

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
 import "./style.css";
 
 import icone_petgato from '../../assets/gatinho_petgato_branco.svg';
-import { Link } from "@reach/router";
+import { Link } from 'react-router-dom';
 
 export default function Header(){
     return(
         <div className="header">
-            <img alt="PetGatô" src={icone_petgato}/>
+            <Link to="/"><img alt="PetGatô" src={icone_petgato}/></Link>
             <div className="links">
                 <Link to="/">Página Inicial</Link>
                 <Link to="/sobre">Sobre Nós</Link>
