406c34fb9509e4e5a781c9b6d319fa36b036b308
Victor Manuel <victormanuel@cjr.org.br>
2021-01-12 00:14:33 -0300

    estilo: adiciona medidas relativas

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
 import "./style.css";
 
-import Icon from '../../assets/gatinho_petgato_branco.svg';
+import icone_petgato from '../../assets/gatinho_petgato_branco.svg';
 import { Link } from "@reach/router";
 
 export default function Header(){
     return(
         <div className="header">
-            <img alt="icon petgato" src={Icon}/>
+            <img alt="PetGatô" src={icone_petgato}/>
             <div className="links">
                 <Link to="/">Página Inicial</Link>
                 <Link to="/sobre">Sobre Nós</Link>
@@ -14,5 +14,5 @@ export default function Header(){
                 <Link to="/login">Entrar</Link>
             </div>
         </div>
-    )
+    );
 }
