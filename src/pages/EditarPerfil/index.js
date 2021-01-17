46e2db1b267ed6e642a9b959697319583e5ce93a
Victor Manuel <victormanuel@cjr.org.br>
2021-01-17 20:52:46 -0300

    refactor: adiciona a página de editar perfil refatorada

--- src/pages/EditarPerfil/index.js
+++ src/pages/EditarPerfil/index.js
@@ -10,38 +10,31 @@ import profile_user_image from "../../assets/images/cintialorenzzo.jpeg";
 const EditarPerfil = () => (
     <div className="container-editar-perfil">
         <div className="header-editar-perfil"><Header atual={4} /></div>
-        <div className="content-editar-perfil">
-            <div className="user-image">
-                <div className="container-user-image"><img src={profile_user_image} /></div>
-                <div className="container-alterar-foto" >
-                    <img src={camera_icon} />
-                    <a>Alterar sua foto de perfil</a>
-                </div>
+        <div className="user-image">
+            <div className="container-user-image">
+                <img src={profile_user_image} />
             </div>
+            <div className="container-alterar-foto" >
+                <img src={camera_icon} />
+                <a>Alterar sua foto de perfil</a>
+            </div>
+        </div>
 
-            <div className="user-info">
-                <div className="title">
-                    <h2>SUA CONTA</h2>
-                    <h1>Edite seu perfil</h1>
-                </div>
-                
-                <div className="first-row">
-                    <Input name="Nome" prevValue={"Cíntia Lorenzzo"} />
-                    <Input name="Email" prevValue={"admin@petgato.com.br"} disabled={true} />
-                </div>
-                
-                <div className="second-row">
-                    <Input name="Nova senha" password={true} />
-                    <Input name="Confirme sua senha" password={true} />
-                </div>
+        <div className="user-info">
+            <div className="title">
+                <h2>SUA CONTA</h2>
+                <h1>Edite seu perfil</h1>
+            </div>
+            <Input name="Nome" prevValue={"Cíntia Lorenzzo"} styles={1} />
+            <Input name="Email" prevValue={"admin@petgato.com.br"} styles={1} disabled={true} />
+            <div>
+                <Input name="Nova senha" password={true} styles={1} />
                 <p>Deixe em branco caso não queira alterar</p>
-
-                <div className="footer">
-                    <Input name="Senha atual" password={true} />
-                    <div><Button styles={1} onClick={() => ( alert('SALVAR') )}>SALVAR</Button></div>
-                </div>
             </div>
-            
+            <Input name="Confirme sua senha" password={true} styles={1} />
+            <Input name="Senha atual" password={true} styles={1} />
+            <Button styles={1} onClick={() => ( alert('SALVAR') )}>SALVAR</Button>
+
         </div>
         <div className="footer-editar-perfil"><Footer /></div>
     </div>
