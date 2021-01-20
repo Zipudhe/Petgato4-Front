cc63ce6a2b915111a42c2133f06819bcd7b01301
Victor Manuel <victormanuel@cjr.org.br>
2021-01-20 00:59:28 -0300

    feat: implementa modal na página de mensagens

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -2,8 +2,6 @@ import './styles.css';
 import close_icon from '../../assets/close_icon.svg';
 
 export default function Modal({ content, close, styles=0 }){
-    // style === 0 ? denúncia : mensagem
-
     // fecha modal ao clicar fora da div
     const verifyClick = (target) => {
         if(target.className === 'modal'){
@@ -15,14 +13,14 @@ export default function Modal({ content, close, styles=0 }){
         <div className="modal" onClick={e => verifyClick(e.target)}>
             <div className="box-modal">
                 <div className="header-modal">
+                    <h2 className="a">Mensagem: {content.name}</h2>
                     <img src={close_icon} onClick={close} />
-                    <h2>Mensagem: </h2>
                 </div>
 
                 <div className="content-modal">
                     <p><b>Email:</b> {content.email}</p>
                     <p><b>Mensagem:</b></p>
-                    <p>{content.description} nao passa pro lado aaaaa</p>
+                    <p>{content.description}</p>
                 </div>
 
             </div>
